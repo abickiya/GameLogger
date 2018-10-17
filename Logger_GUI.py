7150fa790215900fcce7e4f6539c32860d8c8645
Aaqib <abickiya@uci.edu>
2018-10-17 00:42:09 -0700

    Added load and save buttons

--- Logger_GUI.py
+++ Logger_GUI.py
@@ -7,9 +7,19 @@ import tkinter
 class Logger_GUI:
     """ Class running an Othello game """
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.root = tkinter.Tk()
-        self.canvas = tkinter.Canvas(master=self.root, width=500, height=500, background='blue')
+        self.canvas = tkinter.Canvas(master=self.root, width=500, height=500)
+        self.canvas.grid()
+        self.draw_entry()
+
+    def draw_entry(self) -> None:
+        welcome_label = tkinter.Label(master=self.root, text="Welcome to the Game Logger!", font=('Helvetica', 25))
+        load_option = tkinter.Button(master=self.root, text='Load from a file', height=5, width=15)
+        new_option = tkinter.Button(master=self.root, text='Start a new list', height=5, width=15)
+        welcome_label.grid(column=0, row=0, sticky=tkinter.N)
+        load_option.place(relx=.35, rely=.5, anchor='center')
+        new_option.place(relx=.65, rely=.5, anchor='center')
 
     def run(self) -> None:
         self.root.mainloop()
@@ -17,3 +27,10 @@ class Logger_GUI:
 
 game = Logger_GUI()
 game.run()
+
+
+'''
+        self.mode_button = tkinter.Button(master = self.root, text = 'Click to set rules and start game',
+            font = ('Helvetica', 15), command = self._mode_button_pressed)
+            
+'''
