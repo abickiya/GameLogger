67d3544b69e8b4aad9be2663e4502c21b0e6cbdd
Aaqib <abickiya@uci.edu>
2018-10-19 00:58:41 -0700

    Added reaction to pressing load or save, also made the widgets work better when resized

--- Logger_GUI.py
+++ Logger_GUI.py
@@ -5,32 +5,47 @@ from Logger import *
 import tkinter
 
 class Logger_GUI:
-    """ Class running an Othello game """
+    """ """
 
     def __init__(self) -> None:
+        self.game_list = []
         self.root = tkinter.Tk()
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
         self.canvas = tkinter.Canvas(master=self.root, width=500, height=500)
         self.canvas.grid()
         self.draw_entry()
 
     def draw_entry(self) -> None:
-        welcome_label = tkinter.Label(master=self.root, text="Welcome to the Game Logger!", font=('Helvetica', 25))
-        load_option = tkinter.Button(master=self.root, text='Load from a file', height=5, width=15)
-        new_option = tkinter.Button(master=self.root, text='Start a new list', height=5, width=15)
-        welcome_label.grid(column=0, row=0, sticky=tkinter.N)
-        load_option.place(relx=.35, rely=.5, anchor='center')
-        new_option.place(relx=.65, rely=.5, anchor='center')
+        self.welcome_label = tkinter.Label(master=self.root, text="Welcome to the Game Logger!", font=('Helvetica', 25))
+        self.load_option = tkinter.Button(master=self.root, text='Load from a file', height=5, width=15,
+                                          command=self.load_pressed)
+        self.new_option = tkinter.Button(master=self.root, text='Start a new list', height=5, width=15,
+                                         command=self.new_pressed)
+
+        self.welcome_label.grid(column=0, row=0, sticky=tkinter.N)
+        self.load_option.place(relx=.35, rely=.5, anchor='center')
+        self.new_option.place(relx=.65, rely=.5, anchor='center')
 
     def run(self) -> None:
         self.root.mainloop()
 
+    def load_pressed(self) -> None:
+        self.load_option.destroy()
+        self.new_option.destroy()
+        self.welcome_label.destroy()
+        self.game_list = file_reader()
+        self.draw_controls()
+
+    def new_pressed(self) -> None:
+        self.load_option.destroy()
+        self.new_option.destroy()
+        self.welcome_label.destroy()
+        self.draw_controls()
+
+    def draw_controls(self) -> None:
+        print("Hello")
+
 
 game = Logger_GUI()
 game.run()
-
-
-'''
-        self.mode_button = tkinter.Button(master = self.root, text = 'Click to set rules and start game',
-            font = ('Helvetica', 15), command = self._mode_button_pressed)
-            
-'''
