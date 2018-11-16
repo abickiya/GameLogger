c80940f0d9ef9ccbaeca0b6573e5da94e15d3aca
Aaqib <abickiya@uci.edu>
2018-11-15 16:24:17 -0800

    Using a different source, integrating manually, no wrappers

--- Logger_API.py
+++ Logger_API.py
@@ -1,11 +1,36 @@
 
-from igdb_api_python.igdb import igdb
+import urllib.parse
+import urllib.request
+import json
 
-igdb = igdb("99b931869c35e813e368d974594713f5")
+BASE_URL = "https://www.giantbomb.com/api"
+API_KEY = "0d710e3bcc9a40c5d8f67bd785df5f9e85b7fce5"
 
-result = igdb.games({
-    'search': "persona 5",
-    'fields': 'name'})
 
-for game in result.body:
-    print("Retrieved: " + game["name"])
+def build_search_game(g_name: "String") -> "String":
+    url = BASE_URL + "/search/?" + urllib.parse.urlencode([('api_key', API_KEY), ('format', 'json'),
+                                                           ('query', g_name), ('resources', 'game')])
+    return url
+
+
+def get_game_page(g_name: "String") -> json:
+    url = build_search_game(g_name)
+    response = None
+    try:
+        response = urllib.request.urlopen(url)
+        j_text = response.read().decode(encoding='utf-8')
+        return json.loads(j_text)
+
+    finally:
+        if response is not None:
+            response.close()
+
+
+def print_names(j_text: json):
+    results = jtext['results']
+    print()
+    for x in results:
+        print(x['name'])
+
+jtext = get_game_page(input("Enter the game you would like to search for: "))
+print_names(jtext)
